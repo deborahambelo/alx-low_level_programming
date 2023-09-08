b309d10198861ea903da2061437475c380bd8bb1
deborahambelo <ketema.deborah@gmail.com>
2023-09-08 00:22:58 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - Entry point of a code
+ * main - Entry point
  * Return: 1 for success
  */
 int main(void)
 {
-	char my_var[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-
-	write(2, my_var, sizeof(my_var) - 1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 
 }
