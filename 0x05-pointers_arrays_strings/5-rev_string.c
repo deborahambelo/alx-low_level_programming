797936e3ac82e32b99071eeadbf85c4e29758403
deborahambelo <ketema.deborah@gmail.com>
2023-09-18 11:17:52 -0700

    reverses a string.

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,15 +7,17 @@
 void rev_string(char *s)
 {
 	int len = 0;
+	int i;
 
 	while (s[len] != '\0')
 	{
 		len++;
 	}
-	len--;
-	for (; len >= 0; len--)
+	for (i = 0; i < len / 2; i++)
 	{
-		_putchar(s[len]);
+		char temp = s[i];
+
+		s[i] = s[len - 1 - i];
+		s[len - 1 - i] = temp;
 	}
-	_putchar('\n');
 }
