29db5be95018d6f7a25a0117ea419c0b451ab988
deborahambelo <ketema.deborah@gmail.com>
2023-09-18 13:35:31 -0700

    prints half of a string

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -6,27 +6,24 @@
  */
 void puts_half(char *str)
 {
-    int i, count, start;
+	int i, count, start;
 
-    count = 0;
-
-    while (str[count] != '\0')
-    {
-        count++;
-    }
-
-    if (count % 2 == 0) 
-    {
-        start = count / 2; 
-    }
-    else
-    {
-        start = (count + 1) / 2; 
-    }
-
-    for (i = start; str[i] != '\0'; i++)
-    {
-        _putchar(str[i]);
-    }
-    _putchar('\n');
+	count = 0;
+	while (str[count] != '\0')
+	{
+		count++;
+	}
+	if (count % 2 == 0)
+	{
+		start = count / 2;
+	}
+	else
+	{
+		start = (count + 1) / 2;
+	}
+	for (i = start; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
 }
