462446f82c680cf33d0482e1b9c4325f3fd703fd
deborahambelo <ketema.deborah@gmail.com>
2023-09-18 09:54:58 -0700

    prints a string, in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_rev - prints string backwards
+ * @s: string
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	char *str = s;
+
+	while (*s != '\0')
+	{
+		s++;
+	}
+	s--;
+	for (; s >= str; s--)
+	{
+		_putchar(*s);
+	}
+	putchar('\n');
+}
