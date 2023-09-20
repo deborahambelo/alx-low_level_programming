06bd14f2652a488ce1bb0ec37d9ee735dbcbcff0
deborahambelo <ketema.deborah@gmail.com>
2023-09-20 09:31:23 -0700

    concatenates two strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,19 +7,18 @@
  */
 char *_strcat(char *dest, char *src)
 {
-    char *start = dest;
+	char *start = dest;
 
-    while (*dest != '\0')
-    {
-        dest++;
-    }
-    while (*src != '\0')
-    {
-        *dest = *src;
-        dest++;
-        src++;
-    }
-    *dest = '\0';
-
-    return (start);
+	while (*dest != '\0')
+	{
+		dest++;
+	}
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+	return (start);
 }
