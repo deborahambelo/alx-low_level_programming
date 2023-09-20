e7763566ed5940c693008ce5618ef72ff36f688a
deborahambelo <ketema.deborah@gmail.com>
2023-09-20 08:47:09 -0700

    copies a string

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,16 +9,15 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 	char *start = dest;
-    int i;
+	int i;
 
-    for(i = 0; i < n && src[i] != '\0'; i++){
-        dest[i] = src[i];
-
-    }
-     for (; i < n; i++) {
-        dest[i] = '\0';
-    }
-
-
-    return start;
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	for (; i < n; i++)
+	{
+		dest[i] = '\0';
+	}
+	return (start);
 }
