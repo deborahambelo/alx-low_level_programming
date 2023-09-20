741c8e3f4017b0f3bfabdf7946eeb01b7e5ec7c5
deborahambelo <ketema.deborah@gmail.com>
2023-09-20 13:12:32 -0700

    changes all lowercase letters

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,16 +1,19 @@
 #include "main.h"
 /**
- * string_toupper - Function that reverse the content of an array of int
- * @a: Type int
- * Return: string a
+ * *string_toupper - Function that reverses an array
+ * @str: a pointer
+ * Return: string str
  */
-char *string_toupper(char *str) {
-    int i;
-    for (i = 0; str[i] != '\0'; i++) {
-        if (str[i] >= 'a' && str[i] <= 'z') {
+char *string_toupper(char *str)
+{
+	int i;
 
-            str[i] = str[i] - ('a' - 'A');
-        }
-    }
-    return str;
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] = str[i] - ('a' - 'A');
+		}
+	}
+	return (str);
 }
