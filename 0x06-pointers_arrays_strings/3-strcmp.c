2aab4f3487676de06f48cb20ab98c698cc3fbdb3
deborahambelo <ketema.deborah@gmail.com>
2023-09-20 11:46:13 -0700

    compares two strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -6,35 +6,14 @@
  * Return: Always 0.
  */
 int _strcmp(char *s1, char *s2)
+
 {
-    int i;
+	int a;
 
-    for (i = 0; s1[i] != '\0'; i++)
-    {
-        int asc1 = (int)(s1[i]);
-        int asc2 = (int)(s2[i]);
-
-        if (asc1 > asc2)
-        {
-            return 15;
-        }
-        else if (asc1 < asc2)
-        {
-            return -15;
-        }
-    }
-
-   
-    if (s1[i] == '\0' && s2[i] == '\0')
-    {
-        return 0;
-    }
-    else if (s1[i] == '\0')
-    {
-        return -15; 
-    }
-    else
-    {
-        return 15; 
-    }
+	for (a = 0; s1[a] != '\0' && s2[a] != '\0'; a++)
+	{
+		if (s1[a] != s2[a])
+		return (s1[a] - s2[a]);
+	}
+	return (0);
 }
