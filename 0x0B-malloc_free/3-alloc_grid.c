5e5110ba0251f6f1d0bcadecaa9513c71e229b39
deborahambelo <ketema.deborah@gmail.com>
2023-10-07 11:07:42 -0700

    task 3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,38 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "main.h"
+/**
+ * alloc_grid - allocates a grid, make space and free space
+ * @width: takes in width of grid
+ * @height: height of grid
+ * Return: grid with freed spaces
+ */
+int **alloc_grid(int width, int height)
+{
+	int **ipp;
+	int i, j;
 
-int **alloc_grid(int width, int height) {
-    int **ipp;
-    int i, j;
-
-    
-    if (width <= 0 || height <= 0) {
-        return NULL;
-    }
-
-    
-    ipp = (int **)malloc(height * sizeof(int *));
-    if (ipp == NULL) {
-        return NULL;
-    }
-
-    
-    for (i = 0; i < height; i++) {
-        ipp[i] = (int *)malloc(width * sizeof(int));
-        if (ipp[i] == NULL) {
-            
-            for (j = 0; j < i; j++) {
-                free(ipp[j]);
-            }
-            free(ipp);
-            return NULL;
-        }
-
-        
-        for (j = 0; j < width; j++) {
-            ipp[i][j] = 0;
-        }
-    }
-
-    return ipp;
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+	ipp = (int **)malloc(height * sizeof(int *));
+	if (ipp == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < height; i++)
+	{
+		ipp[i] = (int *)malloc(width * sizeof(int));
+		if (ipp[i] == NULL)
+		{
+			for (j = 0; j < i; j++)
+			{
+				free(ipp[j]);
+			}
+			free(ipp);
+			return (NULL);
+		}
+		for (j = 0; j < width; j++)
+		{
+			ipp[i][j] = 0;
+		}
+	}
+	return (ipp);
 }
