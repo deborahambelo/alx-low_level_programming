7222da5a252b7541d6ddcc63ef1ad5ad675bab60
deborahambelo <ketema.deborah@gmail.com>
2023-10-07 10:54:20 -0700

    task 3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -4,23 +4,23 @@
 int **alloc_grid(int width, int height) {
     int **ipp;
     int i, j;
+
     
-   
     if (width <= 0 || height <= 0) {
         return NULL;
     }
 
     
-    ipp = (int **)malloc(width * sizeof(int *));
+    ipp = (int **)malloc(height * sizeof(int *));
     if (ipp == NULL) {
         return NULL;
     }
 
     
-    for (i = 0; i < width; i++) {
-        ipp[i] = (int *)malloc(height * sizeof(int));
+    for (i = 0; i < height; i++) {
+        ipp[i] = (int *)malloc(width * sizeof(int));
         if (ipp[i] == NULL) {
-
+            
             for (j = 0; j < i; j++) {
                 free(ipp[j]);
             }
@@ -28,8 +28,8 @@ int **alloc_grid(int width, int height) {
             return NULL;
         }
 
-       
-        for (j = 0; j < height; j++) {
+        
+        for (j = 0; j < width; j++) {
             ipp[i][j] = 0;
         }
     }
