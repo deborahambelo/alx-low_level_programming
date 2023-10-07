675441b2d853c78adb81abd3fd9f1fd16c8ee133
deborahambelo <ketema.deborah@gmail.com>
2023-10-07 04:33:11 -0700

    task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,55 +1,61 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+/**
+ * str_length - Computes the length of a string.
+ * @s: The string to measure.
+ *
+ * Return: The length of the string.
+ */
+int str_length(char *s) {
+    int length = 0;
+    while (*s != '\0') {
+        length++;
+        s++;
+    }
+    return length;
+}
+/**
+ * str_copy - Copies one string into another.
+ * @dest: The destination string.
+ * @src: The source string.
+ *
+ * Return: A pointer to the null-terminator of the destination string.
+ */
+char *str_copy(char *dest, char *src) {
+    while (*src != '\0') {
+        *dest = *src;
+        dest++;
+        src++;
+    }
+    *dest = '\0';
+    return dest;
+}
+/**
+ * str_concat - Concatenates two strings.
+ * @s1: The first string.
+ * @s2: The second string.
+ *
+ * Return: A pointer to the concatenated string (a new allocation).
+ */
 char *str_concat(char *s1, char *s2) {
-    char *s;
-    int count;
-    char *s1org;
-    char *org;
-    char *start;
-
-	if (s1 == NULL) {
+    if (s1 == NULL) {
         s1 = "";
     }
     if (s2 == NULL) {
         s2 = "";
     }
-    s1org = s1;
-    org = s2;
-    count = 0;
-    while (*s1 != '\0') {
-        count++;
-        s1++;
-    }
-    while (*s2 != '\0') {
-        count++;
-        s2++;
-    }
 
-    s2 = org;
-    s1 = s1org;
-    count++;
-    s = malloc(sizeof(char) * count);
+    int total_length = str_length(s1) + str_length(s2) + 1;
 
-    if (s == NULL) {
+    char *result = malloc(sizeof(char) * total_length);
+
+    if (result == NULL) {
         return NULL;
     }
-start = s;
-    while (*s1 != '\0') {
-        *s = *s1;
-        s++;
-        s1++;
-    }
 
-    while (*s2 != '\0') {
-        *s = *s2;
-        s++;
-        s2++;
-    }
-
-    *s = '\0';
+    char *start = result;
+    result = str_copy(result, s1);
+    str_copy(result, s2);
 
     return start;
 }
-
-
