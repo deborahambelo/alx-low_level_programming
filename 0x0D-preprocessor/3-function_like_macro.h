1eccdc37de0c15512c2e0f1aa342c23d489d040a
deborahambelo <ketema.deborah@gmail.com>
2023-10-09 10:47:21 -0700

    task 3

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,4 +1,4 @@
-#ifndef FUNCTION_LIKE_MACRO_H
-#define FUNCTION_LIKE_MACRO_H
-#define ABS(x) ((x) > (0) ? (x) : (-x))
+#ifndef FUNCTION_ABS_MACRO_H
+#define FUNCTION_ABS_MACRO_H
+#define ABS(x) (x > 0 ? (x) : (x) * -1)
 #endif
