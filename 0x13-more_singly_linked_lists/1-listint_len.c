8b3a100e86428eb291a4430e442eb4a85a2aa3f2
deborahambelo <ketema.deborah@gmail.com>
2023-10-22 23:08:49 -0700

    task 0

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "lists.h"
+#include <stdlib.h>/**
+ * listint_len - returns the number of elements
+ * @h: head of a list.
+ *
+ * Return: numbers of nodes.
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
