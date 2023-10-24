6de21b526c0073390b9cd417a5353dc58ea653eb
deborahambelo <ketema.deborah@gmail.com>
2023-10-24 10:44:21 -0700

    task 5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,12 +8,13 @@
  */
 void free_listint2(listint_t **head)
 {
-  listint_t *tmp;
+	listint_t *tmp;
 
-  while (*head != NULL) {
-    tmp = (*head)->next;
-    free(*head);
-    *head = tmp;
-  }
-  *head = NULL;
+	while (*head != NULL)
+	{
+		tmp = (*head)->next;
+		free(*head);
+		*head = tmp;
+	}
+	*head = NULL;
 }
